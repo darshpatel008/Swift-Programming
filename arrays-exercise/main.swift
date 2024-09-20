104777ceb1f664cb701971d8d5368dbf71edecdf
darshviroja008 <darshviroja12345@gmail.com>
2024-09-20 08:26:43 +0530

    Initial-commit

---
+++ arrays-exercise/main.swift
@@ -0,0 +1,62 @@
+func exercise() {
+
+    let numbers = [45, 73, 195, 53]
+    // 🚨 Don't change the code above 👆
+
+    //####################################
+    //Write your code below this line 👇
+
+   var  firstMultiplication = numbers[0] * numbers[1]
+      var  secondMultiplication = numbers[1] * numbers[2]
+      var  thirdMultiplication = numbers[2] * numbers[3]
+    //var numberTwo = numbers[1]
+
+    //var numberSquare = numberOne * numberTwo
+    
+    let computedNumbers = [ firstMultiplication , secondMultiplication , thirdMultiplication  ]
+                
+    
+    
+    //Replace this comment with your code.
+      
+
+
+    //Write your code above this line 👆
+    //####################################
+
+    //🚨 Don't change the code below 👇
+    print(computedNumbers)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Dont change this code.
+exercise()
